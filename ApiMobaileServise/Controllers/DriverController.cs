6332655876f8c434e17814576568b7ed14158ee4
Novel <Novel@DESKTOP-QSGCUMI>
2019-05-06 16:37:03 +0300

    добавлены контролеры и модели для инспекции драйвера

---
+++ ApiMobaileServise/Controllers/DriverController.cs
@@ -0,0 +1,43 @@
+﻿using ApiMobaileServise.Models;
+using ApiMobaileServise.Servise;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+
+namespace ApiMobaileServise.Controllers
+{
+    [Route("Mobile/Driver")]
+    public class DriverController : Controller
+    {
+        ManagerMobileApi managerMobileApi = new ManagerMobileApi();
+
+        [HttpPost]
+        [Route("CheckInspectionDriver")]
+        public string CheckInspecktionDriver(string token, string idVe, string jsonStrAsk, int type)
+        {
+            string respons = null;
+            if (token == null || token == "")
+            {
+                return JsonConvert.SerializeObject(new ResponseAppS("failed", "1", null));
+            }
+            try
+            {
+                bool isToken = managerMobileApi.CheckToken(token);
+                if (isToken)
+                {
+                    
+                    respons = JsonConvert.SerializeObject(new ResponseAppS("success", "", null));
+                }
+                else
+                {
+                    respons = JsonConvert.SerializeObject(new ResponseAppS("failed", "2", null));
+                }
+            }
+            catch (Exception)
+            {
+                respons = JsonConvert.SerializeObject(new ResponseAppS("failed", "Technical work on the service", null));
+            }
+            return respons;
+        }
+    }
+}
---
+++ MDispatch/MDispatch/Service/DriverInspecktion.cs
@@ -0,0 +1,58 @@
+﻿using MDispatch.Models;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+
+namespace MDispatch.Service
+{
+    public class DriverInspecktion
+    {
+        public int CheckInspectionDriver(string token, ref string description)
+        {
+            IRestResponse response = null;
+            string content = null;
+            try
+            {
+                RestClient client = new RestClient(Config.BaseReqvesteUrl);
+                RestRequest request = new RestRequest("Mobile/Driver/CheckInspectionDriver", Method.POST);
+                client.Timeout = 10000;
+                request.AddHeader("Accept", "application/json");
+                request.AddParameter("token", token);
+                response = client.Execute(request);
+                content = response.Content;
+            }
+            catch (Exception)
+            {
+                return 4;
+            }
+            if (content == "" || response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return 4;
+            }
+            else
+            {
+                return GetData(content, ref description);
+            }
+        }
+
+        private int GetData(string respJsonStr, ref string description)
+        {
+            respJsonStr = respJsonStr.Replace("\\", "");
+            respJsonStr = respJsonStr.Remove(0, 1);
+            respJsonStr = respJsonStr.Remove(respJsonStr.Length - 1);
+            var responseAppS = JObject.Parse(respJsonStr);
+            string status = responseAppS.Value<string>("Status");
+            description = responseAppS.Value<string>("Description");
+            if (status == "success")
+            {
+                return 3;
+            }
+            else
+            {
+                description = responseAppS
+                    .Value<string>("description");
+                return 2;
+            }
+        }
+    }
+}
