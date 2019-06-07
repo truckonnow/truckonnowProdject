35bfd81d50d4463c8b11e5f7f11001f73d7c6849
Novel <Novel@DESKTOP-QSGCUMI>
2019-06-07 20:43:47 +0300

    Добален алгоритм затимнения фото подсказки и изменен цвета с рзмером текста для инспекции трака

--- MDispatch/MDispatch/Vidget/View/CameraPage.xaml.cs
+++ MDispatch/MDispatch/Vidget/View/CameraPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MDispatch.NewElement;
 using MDispatch.Service;
 using MDispatch.Vidget.VM;
+using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using static MDispatch.Service.ManagerDispatchMob;
@@ -19,6 +20,21 @@ namespace MDispatch.Vidget.View
             fullPhotoTruckVM = new FullPhotoTruckVM(managerDispatchMob, idDriver, indexCurrent, Navigation, initDasbordDelegate);
             BindingContext = fullPhotoTruckVM;
             paternPhoto.Source = $"Hint{indexCurrent}.jpg";
+            InitElement();
+        }
+
+        private void InitElement()
+        {
+            TimerCallback tm = new TimerCallback(Transparency);
+            Timer timer = new Timer(tm, null, 3000, Timeout.Infinite);
+        }
+
+        private void Transparency(object state)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await paternPhoto.FadeTo(0.6, 500);
+            });
         }
 
         private async void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
