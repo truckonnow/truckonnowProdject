2bfa4e1f39332be2a442952f216b325594f4b95b
Novel <Novel@DESKTOP-2Q389SA>
2019-07-16 17:31:53 +0300

    bзменены текстовые поля на IOS на одно полсный стиль

---
+++ MDispatch/MDispatch.iOS/NewRender/TextIn/TextRender.cs
@@ -0,0 +1,49 @@
+﻿using CoreAnimation;
+using MDispatch.iOS.NewRender.TextIn;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(TextRender))]
+namespace MDispatch.iOS.NewRender.TextIn
+{
+    public class TextRender : EntryRenderer
+    {
+        protected override async void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BorderStyle = UITextBorderStyle.None;
+
+                var view = (Element as Entry);
+
+                if (view != null)
+                {
+                    view.SizeChanged += Element_SizeChanged;
+                }
+            }
+        }
+
+        void DrawBorder(Entry view)
+        {
+            var borderLayer = new CALayer();
+            borderLayer.MasksToBounds = true;
+            borderLayer.Frame = new CoreGraphics.CGRect(0f, view.Height - 3, view.Width, 1f);
+            borderLayer.BorderColor = Color.FromHex("#000000").ToCGColor();
+            borderLayer.BorderWidth = 1.0f;
+            Control.Layer.AddSublayer(borderLayer);
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+
+        void Element_SizeChanged(object sender, System.EventArgs e)
+        {
+            var view = (Element as Entry);
+            if (view != null)
+            {
+                DrawBorder(view);
+            }
+        }
+    }
+}
