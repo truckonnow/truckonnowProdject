a6bbbf0c950f0cb72ae3082aca351a2aa73fb85c
Roma <C>
2019-07-18 08:17:37 -0700

    123

---
+++ MDispatch/MDispatch.iOS/NewRender/Picker/PikerCustom.cs
@@ -0,0 +1,26 @@
+﻿using CoreAnimation;
+using MDispatch.iOS.NewRender.Picker;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(PikerCustom))]
+
+namespace MDispatch.iOS.NewRender.Picker
+{
+    public class PikerCustom : PickerRenderer
+    {
+        public PikerCustom()
+        {
+        }
+
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (this.Control == null) return;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
