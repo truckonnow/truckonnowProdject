75427308cf6cf6cf8e593f653087319f380e027b
Novel <Novel@DESKTOP-2Q389SA>
2019-07-25 17:10:55 +0300

    123

--- MDispatch/MDispatch/Service/Config.cs
+++ MDispatch/MDispatch/Service/Config.cs
@@ -2,6 +2,6 @@
 {
     public static class Config
     {
-        public static string BaseReqvesteUrl { get; private set; } = "http://192.168.0.100:8098";//172.246.126.197
+        public static string BaseReqvesteUrl { get; private set; } = "http://185.172.129.8:8099";//185.172.129.8
     }
 }
