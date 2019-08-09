36b1c72d1e867208e398ae6066b1b7377d661f55
Roma <C>
2019-08-09 03:15:16 -0700

    123

--- MDispatch/MDispatch/Service/Config.cs
+++ MDispatch/MDispatch/Service/Config.cs
@@ -2,6 +2,6 @@ namespace MDispatch.Service
 {
     public static class Config
     {
-        public static string BaseReqvesteUrl { get; private set; } = "http://46.98.160.49:8099";//185.172.129.8 192.168.0.100
+        public static string BaseReqvesteUrl { get; private set; } = "http://46.98.112.202:8099";//185.172.129.8 192.168.0.100
     }
 }
