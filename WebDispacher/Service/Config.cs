7369343dfe0675be52dd98c8edd7e4db200f1bf1
Novel <Novel@DESKTOP-2Q389SA>
2019-08-09 11:21:02 +0300

    123

--- WebDispacher/Service/Config.cs
+++ WebDispacher/Service/Config.cs
@@ -2,6 +2,6 @@
 {
     public static class Config
     {
-        public static string BaseReqvesteUrl { get; private set; } = "http://185.172.129.8";
+        public static string BaseReqvesteUrl { get; private set; } = "http://192.168.0.101:8089";
     }
 }
