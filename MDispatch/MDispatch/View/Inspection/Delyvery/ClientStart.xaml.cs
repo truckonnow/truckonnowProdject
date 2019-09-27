be4f8917886a6ce3b29cd8514eefb89f695db3e6
Novel <Novel@DESKTOP-2Q389SA>
2019-09-27 19:55:49 +0300

    добалена страница пуск для клиента на деливери

---
+++ MDispatch/MDispatch/View/Inspection/Delyvery/ClientStart.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MDispatch.Models;
+using MDispatch.Service;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static MDispatch.Service.ManagerDispatchMob;
+
+namespace MDispatch.View.Inspection.Delyvery
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ClientStart : ContentPage
+    {
+
+        public ClientStart(ManagerDispatchMob managerDispatchMob, string idShip, InitDasbordDelegate initDasbordDelegate, string onDeliveryToCarrier, string totalPaymentToCarrier)
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
