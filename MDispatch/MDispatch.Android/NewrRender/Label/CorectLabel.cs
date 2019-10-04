cfe72c8b29de18fb33082e5ac02a7286cf00f046
Novel <Novel@DESKTOP-2Q389SA>
2019-10-04 21:18:37 +0300

    Везде у надписях переведены елементы(Label) на большие буквы у андроида и IOS

---
+++ MDispatch/MDispatch.Android/NewrRender/Label/CorectLabel.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using MDispatch.Droid.NewrRender.Label;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CorectLabel))]
+namespace MDispatch.Droid.NewrRender.Label
+{
+    public class CorectLabel : LabelRenderer
+    {
+        public CorectLabel(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Label> e)
+        {
+            base.OnElementChanged(e);
+            var element = this.Element as Xamarin.Forms.Label;
+            if (element != null)
+            {
+                this.Element.Text = this.Element.Text.ToUpper();
+            }
+        }
+    }
+}
---
+++ MDispatch/MDispatch.iOS/NewRender/Labal/CorectLabel.cs
@@ -0,0 +1,26 @@
+﻿using MDispatch.iOS.NewRender.Labal;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(Label), typeof(CorectLabel))]
+namespace MDispatch.iOS.NewRender.Labal
+{
+    public class CorectLabel : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Label> e)
+        {
+            base.OnElementChanged(e);
+            UpdatePadding();
+        }
+
+        private void UpdatePadding()
+        {
+            var element = this.Element as Xamarin.Forms.Label;
+            if (element != null)
+            {
+                this.Element.Text = this.Element.Text.ToUpper();
+            }
+        }
+    }
+}
