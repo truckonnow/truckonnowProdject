b2b0e7601b733c132e74ababf5579df3b2809372
sd <sad>
2019-11-01 21:41:34 +0300

    213

--- MDispatch/MDispatch/Service/Config.cs
+++ MDispatch/MDispatch/Service/Config.cs
@@ -2,6 +2,6 @@ namespace MDispatch.Service
 {
     public static class Config
     {
-        public static string BaseReqvesteUrl { get; private set; } = "http://192.168.0.100:8099";// 192.168.0.100
+        public static string BaseReqvesteUrl { get; private set; } = "http://46.98.235.67:8099";// 192.168.0.100
     }
 }
