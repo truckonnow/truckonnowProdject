6ce35ef8f1a42c0a632dd100abee398b1e1135d8
Novel <Novel@DESKTOP-2Q389SA>
2019-11-06 22:37:33 +0200

    Подписаны все макеты для инспекции водителя 45 макетов

--- MDispatch/MDispatch/Vidget/VM/TruckCar.cs
+++ MDispatch/MDispatch/Vidget/VM/TruckCar.cs
@@ -17,227 +17,227 @@ namespace MDispatch.Vidget.VM
             {
                 case 1:
                     {
-                        nameTruck = "1"; //"Running cluster 1/45";
+                        nameTruck = "Front of the truck - 1/45"; //"Running cluster 1/45";
                         break;
                     }
                 case 2:
                     {
-                        nameTruck = "2"; //"Trailer Brake level 2/45";
+                        nameTruck = "Front radiator/center of the front bumper of the truck 2/45"; //"Trailer Brake level 2/45";
                         break;
                     }
                 case 3:
                     {
-                        nameTruck = "3"; //"Front Bumper 3/45";
+                        nameTruck = "Right headlight/Right side of the bumper of the truck 3/45"; //"Front Bumper 3/45";
                         break;
                     }
                 case 4:
                     {
-                        nameTruck = "4";//"R headlight 4/45";
+                        nameTruck = "Left headlight/Left side of the bumper of the truck 4/45";//"R headlight 4/45";
                         break;
                     }
                 case 5:
                     {
-                        nameTruck = "5"; //"L headlight 5/45";
+                        nameTruck = "Windshield trucks 5/45"; //"L headlight 5/45";
                         break;
                     }
                 case 6:
                     {
-                        nameTruck = "6"; //"Grille 6/45";
+                        nameTruck = "Front wheel on the passenger side of the truck. Tread test 6/45"; //"Grille 6/45";
                         break;
                     }
                 case 7:
                     {
-                        nameTruck = "7"; //"R F tire with meter and tire pressure 7/45";
+                        nameTruck = "The front of the truck on the passenger side 7/45"; //"R F tire with meter and tire pressure 7/45";
                         break;
                     }
                 case 8:
                     {
-                        nameTruck = "8"; //"L F tire with meter and tire pressure 8/45";
+                        nameTruck = "Front door on the passenger side of the truck 8/45"; //"L F tire with meter and tire pressure 8/45";
                         break;
                     }
                 case 9:
                     {
-                        nameTruck = "9";//"D side  whole side of the truck.gas / def caps 9/45";
+                        nameTruck = "Rear door on the passenger side of the truck 9/45";//"D side  whole side of the truck.gas / def caps 9/45";
                         break;
                     }
                 case 10:
                     {
-                        nameTruck = "10"; //"Driver side rear tires with meter 10/45";
+                        nameTruck = "Rear inner tire 10/45"; //"Driver side rear tires with meter 10/45";
                         break;
                     }
                 case 11:
                     {
-                        nameTruck = "11";//"Driver side rear bumper corner 11/45";
+                        nameTruck = "rear outer tire 11/45";//"Driver side rear bumper corner 11/45";
                         break;
                     }
                 case 12:
                     {
-                        nameTruck = "12";//"Locked tool box with keys 12/45";
+                        nameTruck = "Rear of the truck on the passenger side 12/45";//"Locked tool box with keys 12/45";
                         break;
                     }
                 case 13:
                     {
-                        nameTruck = "13";//"Full gas canister 13/45";
+                        nameTruck = "Rear right side of truck bumper 13/45";//"Full gas canister 13/45";
                         break;
                     }
                 case 14:
                     {
-                        nameTruck = "14"; //"Full diesel canister 14/45";
+                        nameTruck = "Luggage carrier 14/45"; //"Full diesel canister 14/45";
                         break;
                     }
                 case 15:
                     {
-                        nameTruck = "15";//"Whole bed space 15/45";
+                        nameTruck = "Toolbox 15/45";//"Whole bed space 15/45";
                         break;
                     }
                 case 16:
                     {
-                        nameTruck = "16"; //"Rear trunk lid 16/45";
+                        nameTruck = "Diesel canister 16/45"; //"Rear trunk lid 16/45";
                         break;
                     }
                 case 17:
                     {
-                        nameTruck = "17";//"Passenger side bumper corner 17/45";
+                        nameTruck = "Gas canister 17/45";//"Passenger side bumper corner 17/45";
                         break;
                     }
                 case 18:
                     {
-                        nameTruck = "18";//"Passenger side dually tires #1 with meter 18/45";
-                        break;
-                    }
-                case 19:
-                    {
-                        nameTruck = "19";//"Passenger side dually tires #2 with meter 19/45";
+                        nameTruck = "The front of the truck on the driver’s side 18/45";//"Passenger side dually tires #1 with meter 18/45";
                         break;
                     }
                 case 20:
                     {
-                        nameTruck = "20";//"Passenger side whole 20/45";
+                        nameTruck = "Front door on the driver’s side of the truck 20/45";//"Passenger side dually tires #2 with meter 19/45";
+                        break;
+                    }
+                case 19:
+                    {
+                        nameTruck = "Front wheel on the driver’s side of the truck. Tread test 19/45";//"Passenger side whole 20/45";
                         break;
                     }
                 case 21:
                     {
-                        nameTruck = "21";//"Passenger front tires with meter 21/45";
+                        nameTruck = "Outer rear tire 21/45";//"Passenger front tires with meter 21/45";
                         break;
                     }
                 case 22:
                     {
-                        nameTruck = "22";//"Windshield 22/45";
+                        nameTruck = "Inner rear tire 22/45";//"Windshield 22/45";
                         break;
                     }
                 case 23:
                     {
-                        nameTruck = "23";//"Whole engine bay 23/45";
+                        nameTruck = "Rear of the truck on the driver’s side 23/45";//"Whole engine bay 23/45";
                         break;
                     }
                 case 24:
                     {
-                        nameTruck = "24";//"Coolant level 24/45";
+                        nameTruck = "Truck gas tank 24/45";//"Coolant level 24/45";
                         break;
                     }
                 case 25:
                     {
-                        nameTruck = "25";//"Transmission level 25/45";
+                        nameTruck = "Rear left side of truck bumper 25/45";//"Transmission level 25/45";
                         break;
                     }
                 case 26:
                     {
-                        nameTruck = "26";//"Engine oil level 26/45";
+                        nameTruck = "Mounting the truck and trailer from above 26/45";//"Engine oil level 26/45";
                         break;
                     }
                 case 27:
                     {
-                        nameTruck = "27";//"Engine oil cap 27/45";
+                        nameTruck = "Fastening truck and trailer on the drivers  side (in a connected form) 27/45";//"Engine oil cap 27/45";
                         break;
                     }
                 case 28:
                     {
-                        nameTruck = "28";//"Pin 28/45";
+                        nameTruck = "Fastening truck and trailer on the passenger side (in a connected form) 28/45";//"Pin 28/45";
                         break;
                     }
                 case 29:
                     {
-                        nameTruck = "29";//"Chains 29/45";
+                        nameTruck = "Fastening truck and trailer on the right side (in a unconnected form) 29/45";//"Chains 29/45";
                         break;
                     }
                 case 30:
                     {
-                        nameTruck = "30"; //"Ball 30/45";
+                        nameTruck = "Front left corner of the trailer 30/45"; //"Ball 30/45";
                         break;
                     }
                 case 31:
                     {
-                        nameTruck = "31";//"Break away cable 31/45";
+                        nameTruck = "Right side of the trailer 31/45";//"Break away cable 31/45";
                         break;
                     }
                 case 32:
                     {
-                        nameTruck = "32";//"Coupler l side 32/45";
+                        nameTruck = "Front right wheel of the trailer. Tread test 32/45";//"Coupler l side 32/45";
                         break;
                     }
                 case 33:
                     {
-                        nameTruck = "33";//"Coupler r side 33/45";
+                        nameTruck = "Center right wheel of the trailer. Tread test 33/45";//"Coupler r side 33/45";
                         break;
                     }
                 case 34:
                     {
-                        nameTruck = "34";//"Whole r side trailer (with visible locks on all doors) 34/45";
+                        nameTruck = "Rear right wheel of the trailer 34/45";//"Whole r side trailer (with visible locks on all doors) 34/45";
                         break;
                     }
                 case 35:
                     {
-                        nameTruck = "35";//"#1 tire with meter 35/45";
+                        nameTruck = "Rear right corner of the trailer 35/45";//"#1 tire with meter 35/45";
                         break;
                     }
                 case 36:
                     {
-                        nameTruck = "36";//"#2 tire with meter 36/45";
+                        nameTruck = "Trailer rear 36/45";//"#2 tire with meter 36/45";
                         break;
                     }
                 case 37:
                     {
-                        nameTruck = "37";//"#3 tire with meter 37/45";
+                        nameTruck = "Rear ramp 37/45";//"#3 tire with meter 37/45";
                         break;
                     }
                 case 38:
                     {
-                        nameTruck = "38"; //"Trailer rear l corner 38/45";
+                        nameTruck = "Tread test 38/45"; //"Trailer rear l corner 38/45";
                         break;
                     }
                 case 39:
                     {
-                        nameTruck = "39";//"Rear ramp (with Locks) 39/45";
+                        nameTruck = "Rear left corner of the trailer 39/45";//"Rear ramp (with Locks) 39/45";
                         break;
                     }
                 case 40:
                     {
-                        nameTruck = "40";//"Interior light 40/45";
+                        nameTruck = "Rear left wheel of the trailer 40/45";//"Interior light 40/45";
                         break;
                     }
                 case 41:
                     {
-                        nameTruck = "41";//"Minimum 8 working  straps ( Together with ratchets ) . 4 extra new straps 41/45";
+                        nameTruck = "Center left wheel of the trailer. Tread test 41/45";//"Minimum 8 working  straps ( Together with ratchets ) . 4 extra new straps 41/45";
                         break;
                     }
                 case 42:
                     {
-                        nameTruck = "42";//"Vacuumed floor 42/45";
+                        nameTruck = "Front left wheel of the trailer. Tread test 42/45";//"Vacuumed floor 42/45";
                         break;
                     }
                 case 43:
                     {
-                        nameTruck = "43";//"Both ramps on the door with strap 43/45";
+                        nameTruck = "Left side of the trailer 43/45";//"Both ramps on the door with strap 43/45";
                         break;
                     }
                 case 44:
                     {
-                        nameTruck = "44";//"Trailer rear right corner with lights on 44/45";
+                        nameTruck = "Front right corner of the trailer 44/45";//"Trailer rear right corner with lights on 45/45";
                         break;
                     }
                 case 45:
                     {
-                        nameTruck = "45";//"Plate # 45/45";
+                        nameTruck = "Fastening truck and trailer on the left side (in a unconnected form) 45/45";//"Plate # 45/45";
                         break;
                     }
             }
