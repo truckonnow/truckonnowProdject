00d398420d80725e90f995cce29ec472b6284b81
sd <sad>
2019-12-04 18:01:19 +0300

    Добалено типы камеры и применение типа камеры

--- MDispatch/MDispatch.iOS/NewRender/CustomCamera/CameraPageRender.cs
+++ MDispatch/MDispatch.iOS/NewRender/CustomCamera/CameraPageRender.cs
@@ -21,6 +21,7 @@ namespace MDispatch.iOS.NewRender.CustomCamera
         AVCaptureStillImageOutput stillImageOutput;
         AVCaptureVideoPreviewLayer videoPreviewLayer = null;
         UIPaintCodeButton takePhotoButton;
+        UIButton scanPhotoButton;
         UIView liveCameraStream;
         private Timer timer = null;
         private bool isTake = false;
@@ -49,7 +50,18 @@ namespace MDispatch.iOS.NewRender.CustomCamera
         {
             InvokeOnMainThread(() =>
             {
+                if (((CameraPage)Element).TypeCamera == null || ((CameraPage)Element).TypeCamera == "Photo")
+                {
                     View.Add(takePhotoButton);
+                }
+                else if(((CameraPage)Element).TypeCamera == "DetectText")
+                {
+                    View.Add(scanPhotoButton);
+                }
+                else
+                {
+                    View.Add(takePhotoButton);
+                }
             });
         }
 
@@ -144,8 +156,6 @@ namespace MDispatch.iOS.NewRender.CustomCamera
                 {
                     takePhotoButton.Enabled = false;
                     isTake = true;
-                    int width = 1280;
-                    int height = 720;
                     var data = await CapturePhoto();
                     if (data != null)
                     {
@@ -155,6 +165,23 @@ namespace MDispatch.iOS.NewRender.CustomCamera
                     }
                     isTake = false;
                     takePhotoButton.Enabled = true;
+                }
+            };
+
+            scanPhotoButton.TouchUpInside += async (s, e) =>
+            {
+                if (!isTake)
+                {
+                    scanPhotoButton.Enabled = false;
+                    isTake = true;
+                    var data = await CapturePhoto();
+                    if (data != null)
+                    {
+                        UIImage originalImage = ImageFromByteArray(data.ToArray());
+                        (Element as CameraPage).SetScan(originalImage.AsJPEG(.7f).ToArray(), (int)originalImage.Size.Width, (int)originalImage.Size.Height);
+                    }
+                    isTake = false;
+                    scanPhotoButton.Enabled = true;
                 }
             };
         }
@@ -218,6 +245,13 @@ namespace MDispatch.iOS.NewRender.CustomCamera
             {
                 Frame = new CGRect(rightButtonX, bottomButtonY, buttonWidth, buttonHeight)
             };
+            scanPhotoButton = new UIButton()
+            {
+                Frame = new CGRect(rightButtonX, bottomButtonY, buttonWidth, buttonHeight),
+               
+            };
+            scanPhotoButton.SetTitle("Scan", UIControlState.Normal);
+            scanPhotoButton.TitleLabel.Text = "Scan";
             View.InsertSubview(liveCameraStream, 0);
         }
 
@@ -242,6 +276,37 @@ namespace MDispatch.iOS.NewRender.CustomCamera
                 device.WhiteBalanceMode = AVCaptureWhiteBalanceMode.ContinuousAutoWhiteBalance;
                 device.UnlockForConfiguration();
             }
+        }
+
+        private void DrawScanPhotoButton(CGRect frame)
+        {
+            var color = UIColor.White;
+            var bezierPath = new UIBezierPath();
+            bezierPath.MoveTo(new CGPoint(frame.GetMinX() + 0.50000f * frame.Width, frame.GetMinY() + 0.08333f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.27302f * frame.Width, frame.GetMinY() + 0.15053f * frame.Height), new CGPoint(frame.GetMinX() + 0.41628f * frame.Width, frame.GetMinY() + 0.08333f * frame.Height), new CGPoint(frame.GetMinX() + 0.33832f * frame.Width, frame.GetMinY() + 0.10803f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.08333f * frame.Width, frame.GetMinY() + 0.50000f * frame.Height), new CGPoint(frame.GetMinX() + 0.15883f * frame.Width, frame.GetMinY() + 0.22484f * frame.Height), new CGPoint(frame.GetMinX() + 0.08333f * frame.Width, frame.GetMinY() + 0.35360f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.50000f * frame.Width, frame.GetMinY() + 0.91667f * frame.Height), new CGPoint(frame.GetMinX() + 0.08333f * frame.Width, frame.GetMinY() + 0.73012f * frame.Height), new CGPoint(frame.GetMinX() + 0.26988f * frame.Width, frame.GetMinY() + 0.91667f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.91667f * frame.Width, frame.GetMinY() + 0.50000f * frame.Height), new CGPoint(frame.GetMinX() + 0.73012f * frame.Width, frame.GetMinY() + 0.91667f * frame.Height), new CGPoint(frame.GetMinX() + 0.91667f * frame.Width, frame.GetMinY() + 0.73012f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.50000f * frame.Width, frame.GetMinY() + 0.08333f * frame.Height), new CGPoint(frame.GetMinX() + 0.91667f * frame.Width, frame.GetMinY() + 0.26988f * frame.Height), new CGPoint(frame.GetMinX() + 0.73012f * frame.Width, frame.GetMinY() + 0.08333f * frame.Height));
+            bezierPath.ClosePath();
+            bezierPath.MoveTo(new CGPoint(frame.GetMinX() + 1.00000f * frame.Width, frame.GetMinY() + 0.50000f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.50000f * frame.Width, frame.GetMinY() + 1.00000f * frame.Height), new CGPoint(frame.GetMinX() + 1.00000f * frame.Width, frame.GetMinY() + 0.77614f * frame.Height), new CGPoint(frame.GetMinX() + 0.77614f * frame.Width, frame.GetMinY() + 1.00000f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.00000f * frame.Width, frame.GetMinY() + 0.50000f * frame.Height), new CGPoint(frame.GetMinX() + 0.22386f * frame.Width, frame.GetMinY() + 1.00000f * frame.Height), new CGPoint(frame.GetMinX() + 0.00000f * frame.Width, frame.GetMinY() + 0.77614f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.19894f * frame.Width, frame.GetMinY() + 0.10076f * frame.Height), new CGPoint(frame.GetMinX() + 0.00000f * frame.Width, frame.GetMinY() + 0.33689f * frame.Height), new CGPoint(frame.GetMinX() + 0.07810f * frame.Width, frame.GetMinY() + 0.19203f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 0.50000f * frame.Width, frame.GetMinY() + 0.00000f * frame.Height), new CGPoint(frame.GetMinX() + 0.28269f * frame.Width, frame.GetMinY() + 0.03751f * frame.Height), new CGPoint(frame.GetMinX() + 0.38696f * frame.Width, frame.GetMinY() + 0.00000f * frame.Height));
+            bezierPath.AddCurveToPoint(new CGPoint(frame.GetMinX() + 1.00000f * frame.Width, frame.GetMinY() + 0.50000f * frame.Height), new CGPoint(frame.GetMinX() + 0.77614f * frame.Width, frame.GetMinY() + 0.00000f * frame.Height), new CGPoint(frame.GetMinX() + 1.00000f * frame.Width, frame.GetMinY() + 0.22386f * frame.Height));
+            bezierPath.ClosePath();
+            color.SetFill();
+            bezierPath.Fill();
+            UIColor.Black.SetStroke();
+            bezierPath.LineWidth = 1.0f;
+            bezierPath.Stroke();
+            var ovalPath = UIBezierPath.FromOval(new CGRect(frame.GetMinX() + NMath.Floor(frame.Width * 0.12500f + 0.5f), frame.GetMinY() + NMath.Floor(frame.Height * 0.12500f + 0.5f), NMath.Floor(frame.Width * 0.87500f + 0.5f) - NMath.Floor(frame.Width * 0.12500f + 0.5f), NMath.Floor(frame.Height * 0.87500f + 0.5f) - NMath.Floor(frame.Height * 0.12500f + 0.5f)));
+            color.SetFill();
+            ovalPath.Fill();
+            UIColor.Black.SetStroke();
+            ovalPath.LineWidth = 1.0f;
+            ovalPath.Stroke();
         }
 
         private void DrawTakePhotoButton(CGRect frame)
