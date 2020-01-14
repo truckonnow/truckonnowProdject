71454a80a873665e7218de63f18c0bf4f35007bf
Рома <@DESKTOP-BH9GPT6>
2020-01-14 20:05:25 +0200

    123

--- MDispatch/MDispatch/Service/Config.cs
+++ MDispatch/MDispatch/Service/Config.cs
@@ -2,7 +2,7 @@ namespace MDispatch.Service
 {
     public static class Config
     {
-        public static string BaseReqvesteUrl { get; private set; } = "http://192.168.31.15:8099";//192.168.0.100 212.224.113.5 46.98.235.67
+        public static string BaseReqvesteUrl { get; private set; } = "http://212.224.113.5:8099";//192.168.0.100 212.224.113.5 46.98.235.67
 
     }
 }               
