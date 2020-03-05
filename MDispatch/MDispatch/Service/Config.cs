f1dfe9010579cb4022cdbeb4b19a02334e942233
chuprina.r.v@gmail.com <Polkilo123!!>
2020-03-05 16:57:34 +0300

    11

--- MDispatch/MDispatch/Service/Config.cs
+++ MDispatch/MDispatch/Service/Config.cs
@@ -2,7 +2,8 @@ namespace MDispatch.Service
 {
     public static class Config
     {
-        public static string BaseReqvesteUrl { get; private set; } = "http://192.168.31.44:8099";//192.168.0.100 212.224.113.5 46.98.235.67 192.168.31.44 
+        public static string BaseReqvesteUrl { get; private set; } = "http://212.224.113.5:8099"; //192.168.0.100 212.224.113.5 46.98.235.67 192.168.31.44 
+        public static string BaseSiteUrl { get; private set; } = "http://truckonnow.com"; //192.168.0.100 212.224.113.5 46.98.235.67 192.168.31.44 http://truckonnow.com/
 
     }
 }               
--- MDispatch/MDispatch/View/PageApp/Settings/Settings.xaml.cs
+++ MDispatch/MDispatch/View/PageApp/Settings/Settings.xaml.cs
@@ -25,9 +25,10 @@ namespace MDispatch.View.PageApp.Settings
             await Navigation.PopModalAsync();
         }
 
+        [Obsolete]
         private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
         {
-            Device.OpenUri(new Uri($"http://192.168.31.44/Doc/{CrossSettings.Current.GetValueOrDefault("IdDriver", "0")}"));
+            Device.OpenUri(new Uri($"{Config.BaseSiteUrl}/Doc/{CrossSettings.Current.GetValueOrDefault("IdDriver", "0")}"));
         }
 
         private void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
@@ -35,9 +36,10 @@ namespace MDispatch.View.PageApp.Settings
 
         }
 
+        [Obsolete]
         private void Button_Clicked(object sender, EventArgs e)
         {
-            Device.OpenUri(new Uri($"http://192.168.31.44/Doc?truckPlate={settingsMV.PlateTruck1}&trailerPlate={settingsMV.PlateTrailer1}"));
+            Device.OpenUri(new Uri($"{Config.BaseSiteUrl}Doc?truckPlate={settingsMV.PlateTruck1}&trailerPlate={settingsMV.PlateTrailer1}"));
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
