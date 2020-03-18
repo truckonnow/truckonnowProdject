600c503f8438a67b1f72077684b048b05dba19a6
Рома <@DESKTOP-BH9GPT6>
2020-03-18 19:55:13 +0200

    Модернизация и настройка модернизации сканера плейт номеров по путям;

--- ApiMobaileServise/Servise/GoogleApi/DerectTruck.cs
+++ ApiMobaileServise/Servise/GoogleApi/DerectTruck.cs
@@ -20,7 +20,7 @@ namespace ApiMobaileServise.Servise.GoogleApi
         public void AuchGoole(SqlCommandApiMobile sqlCommandApiMobil)
         {  
             this.sqlCommandApiMobil = sqlCommandApiMobil;
-            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "../AuchConfig/Truckonnow-3fe51d144964.json");
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", Config.AuchGoogleCloud);
         }
 
         public string DetectText(params object[] parames)
