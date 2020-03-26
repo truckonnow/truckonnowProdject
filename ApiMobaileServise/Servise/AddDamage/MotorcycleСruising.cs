09576b3e9eef6617c7153a781a38ea3be6a72acd
Рома <@DESKTOP-BH9GPT6>
2020-03-26 16:00:56 +0200

    Подготовка к алгоритму добаления ущерба для диаграмы круизного мотоцикла;

---
+++ ApiMobaileServise/Servise/AddDamage/MotorcycleСruising.cs
@@ -0,0 +1,110 @@
+﻿using DaoModels.DAO.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ApiMobaileServise.Servise.AddDamage
+{
+    public class MotorcycleСruising : ITypeScan
+    {
+        public int GetCordinatX(string indexPhoto, double x)
+        {
+            int x1 = 0;
+            int[] maxMinForYAndX = GetMaxMinForYAndX(indexPhoto);
+            if (maxMinForYAndX != null)
+            {
+                int differenceX = maxMinForYAndX[2] - maxMinForYAndX[0];
+                x1 = Convert.ToInt32(Math.Round((differenceX * x) + maxMinForYAndX[0], 0));
+            }
+            return x1;
+        }
+
+        public int GetCordinatY(string indexPhoto, double y)
+        {
+            int y1 = 0;
+            int[] maxMinForYAndX = GetMaxMinForYAndX(indexPhoto);
+            if (maxMinForYAndX != null)
+            {
+                int differenceY = maxMinForYAndX[3] - maxMinForYAndX[1];
+                y1 = Convert.ToInt32(Math.Round((differenceY * y) + maxMinForYAndX[1], 0));
+            }
+            return y1;
+        }
+
+        public int[] GetMaxMinForYAndX(string indexPhoto)
+        {
+            int[] maxMinForYAndX = null;
+            switch(indexPhoto)
+            {
+
+            }
+            return maxMinForYAndX;
+        }
+
+        public async Task SetDamage(PhotoInspection photoInspection, string typrCar, string pathScan)
+        {
+            if (photoInspection.Damages != null)
+            {
+                foreach (var damage in photoInspection.Damages)
+                {
+                    Image img1 = Bitmap.FromFile(pathScan);
+                    Image img2 = Bitmap.FromFile($"../Damages/Damage{damage.TypeCurrentStatus}{damage.IndexDamage}.png");
+                    img2 = img2.GetThumbnailImage((int)((double)damage.WidthDamage * 0.30), (int)((double)damage.HeightDamage * 0.30), null, IntPtr.Zero);
+                    Bitmap res = new Bitmap(img1);
+                    Graphics g = Graphics.FromImage(res);
+                    int x = GetCordinatX(photoInspection.IndexPhoto.ToString(), damage.XInterest);
+                    int y = GetCordinatY(photoInspection.IndexPhoto.ToString(), damage.YInterest);
+                    if (photoInspection.IndexPhoto == 0)
+                    {
+                        g.DrawImage(img2, x, y);
+                    }
+                    else if (photoInspection.IndexPhoto == 0)
+                    {
+                        g.DrawImage(img2, y, x);
+                    }
+                    string tempPath = pathScan + "1";
+                    res.Save($"{pathScan.Replace(".jpg", "")}1.jpg");
+                    img1.Dispose();
+                    res.Dispose();
+                    g.Dispose();
+                    img1 = null;
+                    res = null;
+                    g = null;
+                    File.Delete(pathScan);
+                    File.Move($"{pathScan.Replace(".jpg", "")}1.jpg", pathScan);
+                }
+            }
+        }
+
+        public async Task SetDamage(List<DamageForUser> damageForUsers, string typrCar, string pathScan)
+        {
+            if (damageForUsers != null)
+            {
+                foreach (var damage in damageForUsers)
+                {
+                    Image img1 = Bitmap.FromFile(pathScan);
+                    Image img2 = Bitmap.FromFile($"../Damages/Damage{damage.TypeCurrentStatus}{damage.IndexDamage}.jpg");
+                    img2 = img2.GetThumbnailImage(damage.WidthDamage, damage.HeightDamage, null, IntPtr.Zero);
+                    Bitmap res = new Bitmap(img1.Width, img1.Height);
+                    Graphics g = Graphics.FromImage(res);
+                    int x = (int)Math.Round(img1.Width * damage.XInterest, 0);
+                    int y = (int)Math.Round(img1.Height * damage.YInterest, 0);
+                    g.DrawImage(img1, 0, 0);
+                    g.DrawImage(img2, x, y);
+                    string tempPath = pathScan + "1";
+                    res.Save($"{pathScan.Replace(".jpg", "")}1.jpg");
+                    img1.Dispose();
+                    res.Dispose();
+                    g.Dispose();
+                    img1 = null;
+                    res = null;
+                    g = null;
+                    File.Delete(pathScan);
+                    File.Move($"{pathScan.Replace(".jpg", "")}1.jpg", pathScan);
+                }
+            }
+        }
+    }
+}
