14f1c6482d052112491663226d1dce0d995cbb0b
Рома <@DESKTOP-BH9GPT6>
2020-04-17 14:25:55 +0300

    Доаблены алгоритмы добаления ущерба на макет трицикла для макетов 13, 14, 15, 16, 17, 18, 19, 20, 22, 23;

--- ApiMobaileServise/Servise/AddDamage/TricycleMotorcycle.cs
+++ ApiMobaileServise/Servise/AddDamage/TricycleMotorcycle.cs
@@ -44,9 +44,19 @@ namespace ApiMobaileServise.Servise.AddDamage
                 case "7": maxMinForYAndX = new int[] { 700, 25, 853, 178 }; break;
                 case "8": maxMinForYAndX = new int[] { 235, 815, 372, 995 }; break;
                 case "9": maxMinForYAndX = new int[] { 315, 875, 480, 995 }; break;
-                //case "10": maxMinForYAndX = new int[] { 315, 875, 480, 995 }; break;
+                case "10": maxMinForYAndX = new int[] { 300, 775, 495, 895 }; break;
                 case "11": maxMinForYAndX = new int[] { 423, 815, 560, 995 }; break;
                 case "12": maxMinForYAndX = new int[] { 235, 945, 560, 995 }; break;
+                case "13": maxMinForYAndX = new int[] { 635, 582, 880, 777 }; break;
+                case "14": maxMinForYAndX = new int[] { 700, 619, 853, 777 }; break;
+                case "15": maxMinForYAndX = new int[] { 578, 564, 622, 625 }; break;
+                case "16": maxMinForYAndX = new int[] { 430, 569, 695, 747 }; break;
+                case "17": maxMinForYAndX = new int[] { 220, 560, 490, 740 }; break;
+                case "18": maxMinForYAndX = new int[] { 190, 615, 340, 765 }; break;
+                case "19": maxMinForYAndX = new int[] { 430, 90, 515, 155 }; break;
+                case "20": maxMinForYAndX = new int[] { 252, 100, 555, 255 }; break;
+                case "22": maxMinForYAndX = new int[] { 190, 35, 340, 185 }; break;
+                case "23": maxMinForYAndX = new int[] { 220, 62, 490, 242 }; break;
             }
             return maxMinForYAndX;
         }
@@ -64,11 +74,13 @@ namespace ApiMobaileServise.Servise.AddDamage
                     Graphics g = Graphics.FromImage(res);
                     int x = GetCordinatX(photoInspection.IndexPhoto.ToString(), damage.XInterest);
                     int y = GetCordinatY(photoInspection.IndexPhoto.ToString(), damage.YInterest);
-                    if (photoInspection.IndexPhoto == 4 || photoInspection.IndexPhoto == 5 || photoInspection.IndexPhoto == 6 || photoInspection.IndexPhoto == 7)
+                    if (photoInspection.IndexPhoto == 4 || photoInspection.IndexPhoto == 5 || photoInspection.IndexPhoto == 6 || photoInspection.IndexPhoto == 7 || photoInspection.IndexPhoto == 13 || photoInspection.IndexPhoto == 14
+                        || photoInspection.IndexPhoto == 15 || photoInspection.IndexPhoto == 16 || photoInspection.IndexPhoto == 17 || photoInspection.IndexPhoto == 18 || photoInspection.IndexPhoto == 22 || photoInspection.IndexPhoto == 23)
                     {
                         g.DrawImage(img2, x, y);
                     }
-                    else if (photoInspection.IndexPhoto == 8 || photoInspection.IndexPhoto == 9 || photoInspection.IndexPhoto == 10 || photoInspection.IndexPhoto == 11 || photoInspection.IndexPhoto == 12)
+                    else if (photoInspection.IndexPhoto == 8 || photoInspection.IndexPhoto == 9 || photoInspection.IndexPhoto == 10 || photoInspection.IndexPhoto == 11 || photoInspection.IndexPhoto == 12 || photoInspection.IndexPhoto == 19
+                        || photoInspection.IndexPhoto == 20)
                     {
                         g.DrawImage(img2, y, x);
                     }
